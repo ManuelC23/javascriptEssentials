7d62907d2bb8a00a26d3f0bdf096e0d2dae73282
Manuel Cervantes <cervantesm2395@gmail.com>
2024-03-09 17:00:54 -0500

    Get weather using coordinates

--- weatherReport/weather_report.js
+++ weatherReport/weather_report.js
@@ -17,4 +17,28 @@ function showweatherDetails(event) {
             weatherInfo.innerHTML = `<p>Failed to fetch weather. Please try again.</p>`;
         });
 }
+
+function showweatherCoordinates(event) {
+    event.preventDefault();
+    const latitude = document.getElementById('latitude').value;
+    const longitude = document.getElementById('longitude').value;
+    const apiKey = 'c34bbee093737ce102f1b0d950c9b754';
+    const apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${apiKey}`;
+    fetch(apiUrl)
+        .then(response => response.json())
+        .then(data => {
+            const weatherInfo = document.getElementById('weatherInfo');
+            weatherInfo.innerHTML = `<h2>Weather in ${data.name}</h2>
+                              <p>Temperature: ${data.main.temp} &#8451;</p>
+                              <p>Weather: ${data.weather[0].description}</p>`;
+        })
+        .catch(error => {
+            console.error('Error fetching weather:', error);
+            const weatherInfo = document.getElementById('weatherInfo');
+            weatherInfo.innerHTML = `<p>Failed to fetch weather. Please try again.</p>`;
+        });
+}
 document.getElementById('weatherForm').addEventListener('submit', showweatherDetails);
+document.getElementById('weatherFormCoordinates').addEventListener('submit', showweatherCoordinates);
+
+
